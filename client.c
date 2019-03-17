e09e87eaa59f108e6cec7e6b02dbe94a0632c48d
Darius Costolas <dariuscostolas@busymachines.com>
2019-03-17 13:22:49 +0200

    initial commit

---
+++ client.c
@@ -0,0 +1,253 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+
+#define SERVER_PORT 12345
+#define SERVER_IP "127.0.0.1"
+
+#define TRUE 1
+#define FALSE 0
+
+int listenSocket;
+fd_set socks;
+int isAuthenticated = FALSE;
+
+enum PacketTypes
+{
+    Authenticate,
+    SendMessage,
+    AuthenticationAccepted,
+    AuthenticationDenied,
+    BroadcastMessage
+};
+
+typedef struct _Packet
+{
+    int length;
+    int type;
+    int valid;
+    char *data;
+} Packet;
+
+void err(const char *fmt, ...)
+{
+    va_list va;
+    va_start(va, fmt);
+    fprintf(stderr, "error:");
+    vfprintf(stderr, fmt, va);
+    fputc('\n', stderr);
+    va_end(va);
+    exit(-1);
+}
+
+char *currentTime()
+{
+    time_t timer;
+    char *buffer = (char *)malloc(26);
+    struct tm *tm_info;
+
+    time(&timer);
+    tm_info = localtime(&timer);
+
+    strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+    return buffer;
+}
+
+void connectToServer()
+{
+    int rc;
+    listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSocket < 0)
+    {
+        err("socket creation failed!");
+    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    if (connect(listenSocket, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) != 0)
+    {
+        err("Could not connect");
+    }
+
+    printf("Connected to %s:%d.\n", SERVER_IP, SERVER_PORT);
+}
+
+void buildSelectFD()
+{
+    FD_ZERO(&socks);
+    FD_SET(0, &socks);
+    FD_SET(listenSocket, &socks);
+}
+
+void closeConnection()
+{
+    close(listenSocket);
+    FD_CLR(listenSocket, &socks);
+}
+
+void showMessage(Packet *packet)
+{
+    int userLen = packet->data[0];
+    int messageLen = packet->length - userLen - 1;
+    char *username = (char *)malloc(userLen + 1);
+    char *message = (char *)malloc(messageLen + 1);
+    memcpy(username, packet->data + 1, userLen);
+    username[userLen] = 0;
+    memcpy(message, packet->data + 1 + userLen, messageLen);
+    message[messageLen] = 0;
+    char *t = currentTime();
+    printf("\b\b[%s] %s: %s\n", t, username, message);
+}
+
+void recvData()
+{
+    char buffer[1];
+    Packet *packet = (Packet *)malloc(sizeof(Packet));
+    packet->length = 0;
+    packet->valid = TRUE;
+    int firstRecv = TRUE;
+    for (;;)
+    {
+        int receivedBytes = recv(listenSocket, buffer, sizeof(buffer), MSG_DONTWAIT);
+        if (receivedBytes < 0)
+        {
+            if (errno != EWOULDBLOCK)
+            {
+                err("  recv() failed");
+            }
+            break;
+        }
+
+        if (receivedBytes == 0)
+        {
+            printf("Server closed");
+            closeConnection();
+            break;
+        }
+
+        int oldLength = packet->length;
+        packet->length += receivedBytes - (firstRecv ? 1 : 0);
+        if (packet->data == NULL)
+            packet->data = (char *)malloc(packet->length);
+        else
+            packet->data = (char *)realloc(packet->data, packet->length);
+        char *copyTo = packet->data + oldLength;
+        char *copyFrom = buffer + (firstRecv ? 1 : 0);
+        int toRecv = receivedBytes - (firstRecv ? 1 : 0);
+        memcpy(copyTo, copyFrom, toRecv);
+        if (firstRecv)
+        {
+            switch (buffer[0])
+            {
+            case AuthenticationAccepted:
+                isAuthenticated = TRUE;
+                printf("! Authenticated !\n");
+                break;
+            case AuthenticationDenied:
+                printf("! Wrong credentials \n");
+                authenticate();
+                break;
+            case BroadcastMessage:
+                packet->type = BroadcastMessage;
+                break;
+            default:
+                break;
+            }
+        }
+        firstRecv = FALSE;
+    }
+    if (packet->type == BroadcastMessage)
+    {
+        showMessage(packet);
+    }
+}
+
+void sendPacket(Packet *packet)
+{
+    int dataLen = packet->length + 1;
+    char *data = (char *)malloc(dataLen);
+    data[0] = packet->type;
+    memcpy(data + 1, packet->data, dataLen);
+    int rc = send(listenSocket, data, dataLen, 0);
+    if (rc < 0)
+    {
+        perror("  send() failed");
+        closeConnection();
+    }
+    free(packet);
+}
+
+void authenticate()
+{
+    char username[64];
+    char password[64];
+    printf("Username: ");
+    scanf("%s", username);
+    printf("Password: ");
+    scanf("%s", password);
+    getchar();
+    Packet *packet = (Packet *)malloc(sizeof(Packet));
+    packet->type = Authenticate;
+    packet->length = strlen(username) + strlen(password) + 2;
+    packet->data = (char *)malloc(packet->length);
+    packet->data[0] = strlen(username);
+    packet->data[1] = strlen(password);
+    memcpy(packet->data + 2, username, strlen(username));
+    memcpy(packet->data + 2 + strlen(username), password, strlen(password));
+    sendPacket(packet);
+}
+
+int main()
+{
+    connectToServer();
+    authenticate();
+    for (;;)
+    {
+        buildSelectFD();
+        if (isAuthenticated)
+        {
+            printf("# ");
+            fflush(stdout);
+        }
+        int rc = select(listenSocket + 1, &socks, NULL, NULL, NULL);
+        if (rc < 0)
+        {
+            perror("select failed:");
+            break;
+        }
+        if (rc == 0)
+        {
+            perror("select timeout");
+            break;
+        }
+
+        if (FD_ISSET(listenSocket, &socks))
+        {
+            recvData();
+        }
+        else
+        {
+            char buffer[1000];
+            int rc = read(STDIN_FILENO, buffer, 1000);
+            Packet *packet = (Packet *)malloc(sizeof(Packet));
+            int msgLen = rc - 1;
+            packet->data = (char *)malloc(msgLen);
+            memcpy(packet->data, buffer, msgLen);
+            packet->length = msgLen;
+            packet->type = SendMessage;
+            sendPacket(packet);
+        }
+    }
+}
---
+++ server.c
@@ -0,0 +1,469 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+
+#define SAFE_ALLOC(var, type)                         \
+    if ((var = (type *)malloc(sizeof(type))) == NULL) \
+    err("cannot alloc pointer type")
+
+#define SERVER_PORT 12345
+
+#define TRUE 1
+#define FALSE 0
+
+enum PacketTypes
+{
+    Authenticate,
+    SendMessage,
+    AuthenticationAccepted,
+    AuthenticationDenied,
+    BroadcastMessage
+};
+
+typedef struct _Client
+{
+    int socket;
+    char *name;
+    int authenticated;
+    struct _Client *next;
+    struct _Client *previous;
+} Client;
+
+typedef struct _Packet
+{
+    Client *client;
+    int length;
+    int type;
+    int valid;
+    char *data;
+} Packet;
+
+Client *clients = NULL;
+Client *lastClient = NULL;
+
+int len, rc, on = 1;
+int listen_sd, max_sd;
+int desc_ready, end_server = FALSE;
+int close_conn;
+struct sockaddr_in6 addr;
+struct timeval timeout;
+fd_set socks;
+int serverPort = SERVER_PORT;
+
+void err(const char *fmt, ...)
+{
+    va_list va;
+    va_start(va, fmt);
+    fprintf(stderr, "error:");
+    vfprintf(stderr, fmt, va);
+    fputc('\n', stderr);
+    va_end(va);
+    exit(-1);
+}
+
+char *currentTime()
+{
+    time_t timer;
+    char *buffer = (char *)malloc(26);
+    struct tm *tm_info;
+
+    time(&timer);
+    tm_info = localtime(&timer);
+
+    strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+    return buffer;
+}
+
+void initSocket()
+{
+    listen_sd = socket(AF_INET6, SOCK_STREAM, 0);
+    if (listen_sd < 0)
+    {
+        err("socket creation failed!");
+    }
+
+    rc = setsockopt(listen_sd, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on));
+    if (rc < 0)
+    {
+        close(listen_sd);
+        err("setting reusable socket failed");
+    }
+
+    rc = ioctl(listen_sd, FIONBIO, (char *)&on);
+    if (rc < 0)
+    {
+        close(listen_sd);
+        err("socket setting to nonblocking failed");
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin6_family = AF_INET6;
+
+    memcpy(&addr.sin6_addr, &in6addr_any, sizeof(in6addr_any));
+    addr.sin6_port = htons(serverPort);
+
+    rc = bind(listen_sd, (struct sockaddr *)&addr, sizeof(addr));
+    if (rc < 0)
+    {
+        close(listen_sd);
+        err("binding the socket failed");
+    }
+
+    rc = listen(listen_sd, 32);
+    if (rc < 0)
+    {
+        close(listen_sd);
+        err("listen() failed");
+    }
+
+    timeout.tv_sec = 3 * 60;
+    timeout.tv_usec = 0;
+
+    printf("Started listenting on %d\n", serverPort);
+}
+
+void buildSelectFD()
+{
+    FD_ZERO(&socks);
+    FD_SET(listen_sd, &socks);
+    max_sd = listen_sd;
+    Client *client = clients;
+    while (client != NULL)
+    {
+        FD_SET(client->socket, &socks);
+        if (client->socket > max_sd)
+            max_sd = client->socket;
+        client = client->next;
+    }
+}
+
+void acceptNewCLients()
+{
+    int new_sd;
+    do
+    {
+        new_sd = accept(listen_sd, NULL, NULL);
+        if (new_sd < 0)
+        {
+            if (errno != EWOULDBLOCK)
+            {
+                perror("  accept() failed");
+                end_server = TRUE;
+            }
+            break;
+        }
+
+        printf("  New incoming connection - %d\n", new_sd);
+        FD_SET(new_sd, &socks);
+
+        if (clients == NULL)
+        {
+            clients = (Client *)malloc(sizeof(Client));
+            clients->socket = new_sd;
+            clients->authenticated = FALSE;
+            clients->name = NULL;
+            clients->next = NULL;
+            clients->previous = NULL;
+            lastClient = clients;
+        }
+        else
+        {
+            Client *newClient = (Client *)malloc(sizeof(Client));
+            newClient->socket = new_sd;
+            newClient->authenticated = FALSE;
+            newClient->name = NULL;
+            newClient->next = NULL;
+            newClient->previous = lastClient;
+            lastClient->next = newClient;
+            lastClient = newClient;
+        }
+
+        if (new_sd > max_sd)
+            max_sd = new_sd;
+
+    } while (new_sd != -1);
+}
+
+void closeConnection(Client *client)
+{
+    close(client->socket);
+    FD_CLR(client->socket, &socks);
+    if (client->authenticated)
+    {
+        printf("[%s] User %s has closed the connection \n", currentTime(), client->name);
+        fflush(stdout);
+    }
+    if (client->socket == max_sd)
+    {
+        while (FD_ISSET(max_sd, &socks) == FALSE)
+            max_sd -= 1;
+    }
+    if (client == clients)
+    {
+        client->previous = NULL;
+        clients = client->next;
+        if (client == lastClient)
+            lastClient = clients;
+    }
+    else if (client == lastClient)
+    {
+        lastClient = client->previous;
+        lastClient->next = NULL;
+    }
+    else
+        client->previous->next = client->next;
+
+    free(client);
+}
+
+Client *getClient(int socket)
+{
+    Client *client = clients;
+    while (client != NULL)
+    {
+
+        if (client->socket == socket)
+            return client;
+        client = client->next;
+    }
+    return NULL;
+}
+
+void sendPacket(Packet *packet)
+{
+    int dataLen = packet->length + 1;
+    char *data = (char *)malloc(dataLen);
+    data[0] = packet->type;
+    if (packet->data != NULL)
+        memcpy(data + 1, packet->data, dataLen);
+    int rc = send(packet->client->socket, data, dataLen, 0);
+    if (rc < 0)
+    {
+        perror("  send() failed");
+        closeConnection(packet->client);
+    }
+    free(packet);
+}
+
+void authenticateUser(Packet *packet)
+{
+    if (packet->length == 0 || packet->data == NULL)
+    {
+        return;
+    }
+    int usernameLen = packet->data[0];
+    int passwordLen = packet->data[1];
+    char *account = malloc(usernameLen + passwordLen + 2);
+    memcpy(account, packet->data + 2, usernameLen);
+    account[usernameLen] = ':';
+    memcpy(account + usernameLen + 1, packet->data + 2 + usernameLen, passwordLen);
+    account[usernameLen + 1 + passwordLen] = 0;
+    char *line = NULL;
+    FILE *f = fopen("users.db", "r");
+    size_t len;
+    int read;
+    if (f == NULL)
+        err("cannot open users.db");
+    while ((read = getline(&line, &len, f)) != -1)
+    {
+        if (memcmp(line, account, usernameLen + 1 + passwordLen) == 0)
+        {
+            Packet *acceptPacket = (Packet *)malloc(sizeof(Packet));
+            acceptPacket->type = AuthenticationAccepted;
+            acceptPacket->length = 0;
+            acceptPacket->client = packet->client;
+            sendPacket(acceptPacket);
+            packet->client->authenticated = TRUE;
+            char *username = (char *)malloc(usernameLen + 1);
+            memcpy(username, account, usernameLen);
+            username[usernameLen] = 0;
+            packet->client->name = username;
+            printf("[%s] User %s has connected to the server\n", currentTime(), username);
+            fclose(f);
+            free(line);
+            return;
+        }
+    }
+
+    Packet *acceptPacket = (Packet *)malloc(sizeof(Packet));
+    acceptPacket->type = AuthenticationDenied;
+    acceptPacket->length = 0;
+    acceptPacket->client = packet->client;
+    sendPacket(acceptPacket);
+    fclose(f);
+    free(line);
+}
+
+void broadcastMessage(Packet *packet)
+{
+    Client *client = clients;
+    Packet *p = (Packet *)malloc(sizeof(Packet));
+    p->type = BroadcastMessage;
+    int len = packet->length + strlen(packet->client->name) + 1;
+    p->data = (char *)malloc(len);
+    p->data[0] = strlen(packet->client->name);
+    p->client = client;
+    p->length = len;
+    memcpy(p->data + 1, packet->client->name, strlen(packet->client->name));
+    memcpy(p->data + strlen(packet->client->name) + 1, packet->data, packet->length);
+    packet->data[packet->length] = 0;
+    printf("[%s] %s: %s\n", currentTime(), packet->client->name, packet->data);
+    while (client != NULL)
+    {
+        if (client == packet->client)
+        {
+            client = client->next;
+            continue;
+        }
+        p->client = client;
+        sendPacket(p);
+        client = client->next;
+    }
+}
+
+void processPacket(Packet *packet)
+{
+    if (packet->type == Authenticate)
+    {
+        authenticateUser(packet);
+    }
+    else if (packet->type == SendMessage)
+    {
+        broadcastMessage(packet);
+    }
+}
+
+void recvData(Client *client)
+{
+    char buffer[80];
+    Packet *packet = (Packet *)malloc(sizeof(Packet));
+    packet->client = client;
+    packet->length = 0;
+    packet->data = NULL;
+    packet->valid = 1;
+    int firstRecv = TRUE;
+    do
+    {
+        rc = recv(client->socket, buffer, sizeof(buffer), MSG_DONTWAIT);
+        if (rc < 0)
+        {
+            if (errno != EWOULDBLOCK)
+            {
+                perror("  recv() failed");
+                closeConnection(client);
+            }
+            break;
+        }
+
+        if (rc == 0)
+        {
+            closeConnection(client);
+            break;
+        }
+
+        len = rc;
+        int oldLength = packet->length;
+        if (packet->length == 0)
+        {
+            switch (buffer[0])
+            {
+            case Authenticate:
+                packet->type = Authenticate;
+                break;
+            case SendMessage:
+                packet->type = SendMessage;
+                break;
+            default:
+                printf("Invalid packet from client %d", client->socket);
+                packet->valid = 0;
+                break;
+            }
+        }
+        packet->length += len;
+        if (firstRecv)
+            packet->length--;
+        if (packet->data == NULL)
+            packet->data = (char *)malloc(packet->length);
+        else
+            packet->data = (char *)realloc(packet->data, packet->length);
+
+        memcpy(packet->data + oldLength, buffer + (firstRecv), len - (firstRecv));
+
+        firstRecv = FALSE;
+    } while (TRUE);
+    if (packet->valid)
+        processPacket(packet);
+}
+
+void setupPort()
+{
+    size_t size = 256;
+    char *line = (char *)malloc(size);
+    printf("Port (default 12345): ");
+    getline(&line, &size, stdin);
+    if (strlen(line) > 1)
+    {
+        int port = strtol(line, NULL, 10);
+        if(port)
+        {
+            serverPort = port;
+        }
+        
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    setupPort();
+    initSocket();
+    int i;
+    do
+    {
+        buildSelectFD();
+        // printf("Waiting on select()...\n");
+        rc = select(max_sd + 1, &socks, NULL, NULL, NULL);
+        if (rc < 0)
+        {
+            perror("  select() failed");
+            break;
+        }
+        if (rc == 0)
+        {
+            printf("  select() timed out.  End program.\n");
+            break;
+        }
+
+        desc_ready = rc;
+        for (i = 0; i <= max_sd && desc_ready > 0; ++i)
+        {
+            if (FD_ISSET(i, &socks))
+            {
+                desc_ready -= 1;
+
+                if (i == listen_sd)
+                {
+                    acceptNewCLients();
+                }
+                else
+                {
+                    recvData(getClient(i));
+                }
+            }
+        }
+
+    } while (end_server == FALSE);
+
+    for (i = 0; i <= max_sd; ++i)
+    {
+        if (FD_ISSET(i, &socks))
+            close(i);
+    }
+}
